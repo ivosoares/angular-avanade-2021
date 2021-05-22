84a2c275cdcb3774e221956e389e686851434d9b
Ivonaldo Soares <vonaldojrsoares@gmail.com>
2021-05-21 22:37:48 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContadorComponent } from './components/contador/contador.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContadorComponent },
+  { path: 'todo-list', component: TodoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
